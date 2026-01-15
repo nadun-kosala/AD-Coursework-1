0944e91e8e7c83dc78b4fc8c4a524eb4069d3d03
Nadun <Nadun@DESKTOP-MF8QU1G>
2026-01-15 22:02:55 +0530

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,50 @@
+namespace GreenLife_Organic_Store
+{
+    public partial class frmFirst : Form
+    {
+        public frmFirst()
+        {
+            InitializeComponent();
+        }
+
+        private void frmLogin_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabCustomer_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblPassword_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabAdmin_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
