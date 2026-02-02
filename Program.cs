839300dcc2f7e6963e6e4237aa94b73eb26dbd30
Nadun Kosala <nadun.kosala97@gmail.com>
2026-02-02 15:02:15 +0530

    fix: Set the login form to initial start

--- Program.cs
+++ Program.cs
@@ -15,10 +15,7 @@ namespace GreenLife_Organic_Store
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            //Application.Run(new frmCustomerLogin());
-            //Application.Run(new frmCustomerDashboardForm(2));
-            //Application.Run(new frmSearchForm(3));
-            Application.Run(new frmAdminDashboardForm(1));
+            Application.Run(new frmCustomerLogin());
 
         }
     }
